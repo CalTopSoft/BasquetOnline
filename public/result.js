f726252158cd1c71ac57b69047f6822ee3beea13
FlastXStudios <111031492+CalTopSoft@users.noreply.github.com>
2025-06-09 13:15:32 -1200

    Delete public directory

--- public/result.js
+++
@@ -1,8 +0,0 @@
-const Result = ({ winner, name, setScreen }) => {
-    return (
-        <div className="container">
-            <h1>{winner === 'tie' ? '¡Empate!' : winner === name ? '¡Ganaste!' : 'Perdiste'}</h1>
-            <button onClick={() => setScreen('rooms')}>Volver</button>
-        </div>
-    );
-};
